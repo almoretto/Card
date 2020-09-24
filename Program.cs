e92ffd6f7c8bb9a251df5538bd6e98ba15e32608
André Luís Moretto <amoretto@outlook.com.br>
2020-09-24 14:57:13 -0300

    Frist Test

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.IO;
 using CardExtractTreatment.Entities;
-using CardExtractTreatment.Services;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 
 namespace CardExtractTreatment
 {
@@ -19,13 +19,18 @@ namespace CardExtractTreatment
             Console.WriteLine(@"colocado na pasta c:\temp\extrato.csv!");
             Console.WriteLine("=============================================");
             string path = @"" + Console.ReadLine();
+
+            //Instances of lines of the file, and the class that will get the data
             List<string[]> lines = new List<string[]>();
             List<SafraPayEx> sPF = new List<SafraPayEx>();
             SafraPayEx safraPayEx;
+
+            //temp variables to initiate SafraPayEx Class
             int exT, exEC, exPL, exNCAR;
             string aAAAMM, terminal, nSU, produto, modalidade, autori;
             DateTime dataVenda, hora;
             double valorBruto, taxaAdm;
+
             try
             {
                 using FileStream fs = new FileStream(path, FileMode.Open);
@@ -59,30 +64,72 @@ namespace CardExtractTreatment
                     sPF.Add(safraPayEx);
 
                 }
-
-
             }
             catch (IOException e)
             {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
             }
-            /* try
-             {
-                 string[] lines = File.ReadAllLines(sourcePath);
-                 using (StreamWriter sw = File.AppendText(targetPath))
-                 {
-                     foreach (string line in lines)
-                     {
-                         sw.WriteLine(line.ToUpper());
-                     }
-                 }
-             }
-             catch (IOException e)
-             {
-                 Console.WriteLine("An error occurred");
-                 Console.WriteLine(e.Message);
-             }*/
+            try
+            {
+                string targetPath = Path.GetDirectoryName(path) + @"\Proc" + DateTime.Now.ToString("ddMMyyyyhhmm") + ".csv";
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    //Header
+                    sb.Append("T;");
+                    sb.Append("EC;");
+                    sb.Append("AAAAMM;");
+                    sb.Append("Terminal;");
+                    sb.Append("DataVenda;");
+                    sb.Append("Hora;");
+                    sb.Append("NSU;");
+                    sb.Append("Produto;");
+                    sb.Append("Modalidade;");
+                    sb.Append("ExPL;");
+                    sb.Append("ExNCAR;");
+                    sb.Append("ValorBrutoDaParcela;");
+                    sb.Append("TaxaAdm;");
+                    sb.Append("Autorizacao;");
+                    sb.Append("ValorLiquidoDaParcela;");
+                    sb.Append("ParcelaAtual");
+                    sw.WriteLine(sb.ToString());
+                    //Data
+                    foreach (SafraPayEx sp in sPF)
+                    {
+                        
+                        for (int i = 1; i <= sp.ExPL; i++)
+                        {
+                            sb.Append(sp.ExT.ToString() + ";");
+                            sb.Append(sp.ExEC.ToString() + ";");
+                            sb.Append(sp.AAAAMM + ";");
+                            sb.Append(sp.Terminal + ";");
+                            sb.Append(sp.DataVenda.ToString("d", CultureInfo.CurrentCulture) + ";");
+                            sb.Append(sp.Hora.ToString("T", CultureInfo.CurrentCulture) + ";");
+                            sb.Append(sp.NSU + ";");
+                            sb.Append(sp.Produto + ";");
+                            sb.Append(sp.Modalidade + ";");
+                            sb.Append(sp.ExPL.ToString() + ";");
+                            sb.Append(sp.ExNCAR.ToString() + ";");
+                            sb.Append((sp.ValorBruto / sp.ExPL).ToString("F2", CultureInfo.CurrentCulture) + ";");
+                            sb.Append(sp.TaxaAdm.ToString("F2", CultureInfo.CurrentCulture) + ";");
+                            sb.Append(sp.Autori + ";");
+                            sb.Append((sp.ValorLiquido / sp.ExPL).ToString("F2", CultureInfo.CurrentCulture) + ";");
+                            sb.Append(i.ToString() + "/" + sp.ExPL);
+                            sw.WriteLine(sb.ToString());
+                        }
+                        
+                    }
+                    sw.WriteLine("Total de Regiatros" + sPF.Count.ToString());
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
